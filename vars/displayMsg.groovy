534f36d43a51c5f82d57bff0e3cd517b3ae03ecb
lakshp051 <66675935+lakshp051@users.noreply.github.com>
2021-07-24 15:52:58 +0530

    Update displayMsg.groovy

--- vars/displayMsg.groovy
+++ vars/displayMsg.groovy
@@ -1,10 +1,3 @@
-import groovy.transform.Field
-@Field def num = 10
-
 def call() {
-  println(globalVars.msg1)
-  println(globalVars.dir)
-  globalVars.dir = 2
-  println(globalVars.dir)
-  println(num)
+    println(globalVars.msg2)
 }
