101b6a8d0993077cb77e3dd4675dd60583060376
lakshp051 <66675935+lakshp051@users.noreply.github.com>
2021-07-29 20:02:54 +0530

    Update showMessage.groovy

--- src/com/lp/jenkins/showMessage.groovy
+++ src/com/lp/jenkins/showMessage.groovy
@@ -2,8 +2,8 @@ package com.lp.jenkins
 
 import static Constants.USER_HOME;
 
-def showMsg() {
-    println("hello from com.lp.jenkins.showMessage.showMsg())
+class showMessage {
+    def showMsg() {
+        println(USER_HOME)
+    }
 }
-
-return this;
